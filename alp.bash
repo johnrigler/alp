918b5c72858f6fa314129775fab824e63b631878
John Rigler <john@rigler.org>
2018-01-20 13:45:18 -0600

    alp

---
+++ alp.bash
@@ -0,0 +1,19 @@
+alp() {
+
+-() { :; }
+
+# alp is a set of shell scripting conventions
+# which create highly readable and understandable
+# tasks for blockchain consumption
+# It aims to live entirely within a restricted shell
+# environment, which it opens.
+# It also uses colors to denote child shell.  
+# It has eight levels of color below the base
+# and as one descends into these deeper levels
+# then the nature of their tasks changes
+# because blockchain executions will be coming from 
+# base and adopted classes.
+
+bash -r
+
+} ; alp

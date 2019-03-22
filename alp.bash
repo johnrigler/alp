b87c35245cd65b0b35312d653771274f470565de
John Rigler <john@Johns-MacBook-Pro.local>
2019-03-22 14:15:01 -0500

    added more definition and structure

--- alp.bash
+++ alp.bash
@@ -1,19 +1,39 @@
-alp() {
+-() { echo $*; }
+echo welcome from alp
 
--() { :; }
+dc-xf() {
 
-# alp is a set of shell scripting conventions
-# which create highly readable and understandable
-# tasks for blockchain consumption
-# It aims to live entirely within a restricted shell
-# environment, which it opens.
-# It also uses colors to denote child shell.  
-# It has eight levels of color below the base
-# and as one descends into these deeper levels
-# then the nature of their tasks changes
-# because blockchain executions will be coming from 
-# base and adopted classes.
+declare -f $1;
 
-bash -r
+}
+
+dc-f() {
+
+declare -F $1;
+
+}
+
+alp-edit() {
+
+vi ~/alp/alp.bash
+. ~/alp/alp.bash
+} 
+
+alp-edit-bashrc() {
+
+vi ~/.bashrc
+. ~/.bashrc
+}
+
+alp-sum() {
+
+sum | sed 's/\ .*//';
 
 } 
+
+alias ƒ=dc-xf
+alias Ï=dc-f
+alias √=alp-edit
+alias √∫=alp-edit-bashrc
+alias Å=alias
+alias ß=alp-sum

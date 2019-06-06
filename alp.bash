f25f15fad5beafec411c46cb49457fd6b5f3ff9c
root <root@dime.cash>
2019-06-06 09:26:31 -0500

    .

--- alp.bash
+++ alp.bash
@@ -1,39 +1,52 @@
-if [ ! $_ALP_DIR ]; then 
+export _ALP_DIR=~/alp
+export _ALP_SRC=$_ALP_DIR/alp.bash
 
--() { echo $*; } 
-_ALP_DIR=~/alp
-_ALP_SRC=$_ALP_DIR/alp.bash
 
-ƒ() {
+a.e() {
+env | grep '_ALP'
+}
 
+a.f() {
+# ƒ() {
+: https://en.wikipedia.org/wiki/Florin_sign
+: is a shortcut for
 declare -f $1;
 
 }
 
-Ï() {
-
+a.F() {
+# Ï() {
+: https://en.wikipedia.org/wiki/%C3%8F
+: is a shortcut for
 declare -F $1;
 
 }
 
-√() {
+a.v() {
+# √() {
 
+if [ $_ALP_TARGET ]; then
+echo xxxxxxxxxxxx
+vi $_ALP_TARGET
+. $_ALP_TARGET
+else
 vi $_ALP_SRC
 . $_ALP_SRC 
+fi
 } 
 
-√∫() {
+a.vb() {
+# √∫() {
 
 vi ~/.bashrc
 . ~/.bashrc
 }
 
-ß() {
+a.s() {
+# ß() {
 
 sum | sed 's/\ .*//';
 
 } 
 
 . $_ALP_DIR/public/ƒß-38889;
-
-fi
